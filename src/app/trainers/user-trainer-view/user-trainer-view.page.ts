5d1ceba49de3a02fdc0005af697f4f64e69fb8c0
pietroconta <42576699+pietroconta@users.noreply.github.com>
2023-11-02 14:37:28 +0100

    start dev user trainer view

--- src/app/trainers/user-trainer-view/user-trainer-view.page.ts
+++ src/app/trainers/user-trainer-view/user-trainer-view.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/classes';
+import { LsManagerService } from 'src/app/ls-manager.service';
 
 @Component({
   selector: 'app-user-trainer-view',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserTrainerViewPage implements OnInit {
 
-  constructor() { }
-
+  private user!:User;
+  
+  constructor(private lsM:LsManagerService, router:Router) {
+    let pathId = router.url.split("/")[4];
+    var user;
+    let subscribers = lsM.getObj("subscribers");
+    subscribers.data.content.forEach((el:any)=> {
+      if(el.usn_id == pathId){
+        this.user = new User(el.usn_name, el.usn_surname, el.usn_birthdate, el.usn_email,
+        el.usn_username, el.usn_id);
+      }
+    });
+    
+   // let user = lsM.getObj(router.url.split("/")[4]);
+    //user:User = new User();
+   }
+          
   ngOnInit() {
+
   }
 
 }
